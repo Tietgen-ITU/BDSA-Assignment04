45764e9d288b383edd5f9a95d9f8bf8216702c9c
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-10-01 13:44:15 +0200

    Add annotations to models

--- Assignment4.Core/State.cs
+++ Assignment4.Core/State.cs
@@ -2,5 +2,10 @@ namespace Assignment4.Core
 {
     public enum State
     {
+        New,
+        Active, 
+        Resolved, 
+        Closed,
+        Removed
     }
 }
--- Assignment4.Entities/Tag.cs
+++ Assignment4.Entities/Tag.cs
@@ -1,6 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 namespace Assignment4.Entities
 {
+    [Index(nameof(Name), IsUnique = true)]
     public class Tag
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name {get; set;}
+
+        public ICollection<Task> Tasks { get; set; }
     }
 }
