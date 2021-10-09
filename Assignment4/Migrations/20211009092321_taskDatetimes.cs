53b0d058527bd89c0d01c2b10b2cdc08705d3768
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-10-09 11:24:45 +0200

    Add migrations for newly add datetime field

---
+++ Assignment4/Migrations/20211009092321_taskDatetimes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Assignment4.Migrations
+{
+    public partial class taskDatetimes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Tasks",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StateUpdated",
+                table: "Tasks",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "StateUpdated",
+                table: "Tasks");
+        }
+    }
+}
